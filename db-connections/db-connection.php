330e73ee50c8688c7599d44106311bc96fab8f55
ebrahim-elgaml <ebrahim.el.gaml@gmail.com>
2015-10-08 17:34:47 +0200

    remove connection to db

--- db-connections/db-connection.php
+++ db-connections/db-connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$db= mysqli_connect('localhost','root', 'Ee01221838455', 'E-Shop');
+	$db= mysqli_connect('localhost','root', '', 'E-Shop');
 	if ($db->connect_error){ 
 		die("Connection failed: " . $db->connect_error);	
 	} 
